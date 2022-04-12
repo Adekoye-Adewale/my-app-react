d941934244cef0641bad9fd0d084e6c5d88f2f62
AdisaEmpire <94379177+AdisaEmpire@users.noreply.github.com>
2022-04-12 23:36:39 +0100

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
 
-class App extends Component() {
-//   state = {
-//     persons: [
-//       { name: "AKANDA", age: 77 },
-//       { name: "ANIKE", age: 4 },
-//       { name: "ADISA", age: 777 }
-//     ]
+class App extends Component {
+  
+  state = {
+    persons: [
+      { name: "AKANDA", age: 77 },
+      { name: "ANIKE", age: 4 },
+      { name: "ADISA", age: 777 }
+    ]
 
-//   }
+  }
+
+  switchNameHandler = () => {
+    // console.log('was clicked!');
+    this.setState({persons: })
+  }
 
   render () {
     return (
       <div className="App">
         <h1>Hi, I'm React App</h1>
         <p>This is my first trial but i don dey get errors</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} >My position: Kingship</Person>
